a79a2ecf46b242f89343e62f1de28f87b038957e
Ali <125828096+hs7t@users.noreply.github.com>
2025-06-19 11:55:31 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+    import Selector from "./Selector.svelte";
+</script>
+
+<main>
+    <h1>Text mirror</h1>
+    <Selector form={{
+        "message": {
+            "name": "Text",
+            "lead": "Mirror",
+            "type": "text"
+        },
+        "recipient": {
+            "name": "Recipient",
+            "lead": "to",
+            "type": "select",
+            "options": [
+                "Yourself",
+                "Anyone"
+            ]
+        },
+        "time" : {
+            "name": "Time",
+            "type": "tangent_select",
+            "options": {
+                "specific": {
+                    "name": "At a specific time",
+                    "type": "date"
+                },
+                "within_interval": {
+                    "name": "Sometime within an interval",
+                    "type": "date_range"
+                }
+            }
+        },
+    }}></Selector>
+</main>
---
+++ src/routes/Selector.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { get } from "svelte/store";
+
+    let { form } = $props();
+    function getEntries(obj) { return Object.entries(obj) };
+    let formAnswer = {}
+
+    for (let [key, input] of getEntries(form)) {
+        if (input["type"] === "tangent_select") {
+            for (let [key, input] of getEntries(form)) {
+                
+            }
+        } else {
+            formAnswer[key];
+        }
+    }
+</script>
+
+
+<div>
+    {#each getEntries(form) as [key, input]}
+        {input["lead"]}
+        {#if input["type"] === "text"}
+            <input type="text" id={key}>
+        {:else if input["type"] === "select"}
+            <select id={key}>
+                {#each input["options"] as option}
+                    <option>{option}</option>
+                {/each}
+            </select>
+        {:else if input["type"] === "tangent_select"}
+            <select id={key}>
+                {#each getEntries(input["options"]) as [key, tangent]}
+                    <option id={key}>{tangent["name"]}</option>
+                {/each}
+            </select>
+        {/if}
+    {/each}
+</div>
